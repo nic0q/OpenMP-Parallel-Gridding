2d4c043c53323d27f911c8c5dbfc81245731aafb
nicoq <nicolas.farfan.ch@usach.clclear>
2023-11-13 22:54:48 -0300

    [feat]: Speedup^-1

--- gridding.c
+++ gridding.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
   int t = 0, c = 0, N = 0, option, j;
   float deltaX = 0.0, deltaU, deltaV, *absfr, *absfi, *abswt, *fr, *fi, *wt;
   double t1_p, t2_p;
-  char buffer[256], *input_file_name = NULL, *output_file_name = NULL;
+  char buffer[200], *input_file_name = NULL, *output_file_name = NULL;
   
   while ((option = getopt(argc, argv, "i:o:d:N:c:t:")) != -1) {
     switch (option) {
@@ -47,7 +47,7 @@ int main(int argc, char *argv[]) {
   FILE* file = open_file(input_file_name);
   FILE* file2 = open_file(input_file_name);
 
-  if(file == NULL){return -1;}
+  if (file == NULL) {return -1;}
 
   deltaU = 1 / (N * arcsec_to_rad(deltaX));  // to radians
   deltaV = deltaU;
@@ -62,56 +62,62 @@ int main(int argc, char *argv[]) {
 
   t1_p = omp_get_wtime();
   #pragma omp parallel shared(absfr, absfi, abswt, j) firstprivate(fr, fi, wt)
-    {
+  {
     #pragma omp single
     {
     float vr, vi, wk;
-    int cont = 0, it = 0, row, index, pos;
-    for (int i = 0; i < t; i++){
-      #pragma omp task untied shared(file) private(it) shared(cont)
-      while(feof(file) == 0) {
+    int cont = 0, it, row, index, pos;
+    for (int i = 0; i < t; i++) {
+      #pragma omp task untied shared(file, cont) private(it)
+      {
+      while (feof(file) == 0) {
         float* vis = calloc(c * 6, sizeof(float));
         #pragma omp critical // SC
-        for (int j = 0; j < c; j++) {
-          if(feof(file) != 0){
-            break;
+        {
+          for (int j = 0; j < c; j++) {
+            if (feof(file) != 0) {
+              break;
+            }
+            pos = j * 6;
+            fgets(buffer, sizeof(buffer), file);
+            sscanf(buffer, "%f,%f,%*f,%f,%f,%f,%f,%*f", &vis[pos], &vis[pos+1], &vis[pos+2], &vis[pos+3], &vis[pos+4], &vis[pos+5]);
+            if (cont % 500000 == 0) {
+              printf("Reading line: %d\n", cont);
+            }
+            cont++;
           }
-          pos = j * 6;
-          fgets(buffer,sizeof(buffer),file);
-          sscanf(buffer, "%f,%f,%*f,%f,%f,%f,%f,%*f", &vis[pos], &vis[pos+1], &vis[pos+2], &vis[pos+3], &vis[pos+4], &vis[pos+5]);
-          if(cont % 500000 == 0){
-            printf("Reading line: %d\n", cont);
+        } // SC
+        if (vis[0] != 0.0) {
+          for (it = 0; it < c; it++) {
+            vr = vis[it * 6 + 2]; // parte real
+            vi = vis[it * 6 + 3]; // parte im
+            wk = vis[it * 6 + 4]; // peso
+
+            index = grid(it, vis, deltaU, deltaV, N);
+
+            fr[index] += wk * vr;  // acumulate in matrix fr, fi, wt
+            fi[index] += wk * vi;
+            wt[index] += wk;
           }
-          cont++;
-        }  // SC
-        if(vis[0] != 0.0)
-        for(it = 0; it < c; it++){
-          vr = vis[it * 6 + 2]; // parte real
-          vi = vis[it * 6 + 3]; // parte im
-          wk = vis[it * 6 + 4]; // peso
-
-          index = grid(it, vis, deltaU, deltaV, N);
-
-          fr[index] += wk * vr;  // acumulate in matrix fr, fi, wt
-          fi[index] += wk * vi;
-          wt[index] += wk;
-        }
+        } 
       } // while archivo
-    } // for tasks
-  } // single
+      } // tasks
+    } // for
+    } // single
   #pragma omp taskwait
   #pragma omp for
-  for(int i = 0; i < N * N; i++){
+  for (int i = 0; i < N * N; i++) {
     absfr[i] += fr[i];
     absfi[i] += fi[i];
     abswt[i] += wt[i];
   }
   #pragma omp for
-  for (j = 0; j < N * N; j++)
-    if(abswt[j] != 0){ // /0: -inf
+  for (int j = 0; j < N * N; j++) {
+    if (abswt[j] != 0) { // /0: -inf
       absfr[j] = absfr[j] / abswt[j];
       absfi[j] = absfi[j] / abswt[j];
     }
+  }
   } // Parallel
   t2_p = omp_get_wtime();
   printf("Private matrices time: %f [s]\n", t2_p - t1_p);
@@ -158,7 +164,6 @@ int main(int argc, char *argv[]) {
           wk = vis[it * 6 + 4]; // peso
 
           index = grid(it, vis, deltaU, deltaV, N);
-
           #pragma omp critical
           {
             absfr[index] += wk * vr;  // acumulate in matrix fr, fi, wt
